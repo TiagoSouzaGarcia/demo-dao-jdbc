5a0f6610d5c2b658e743d0196fd21b93ae829acf
TiagoSouzaGarcia <tiago.s.garcia@gmail.com>
2021-03-09 15:50:09 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,7 +12,10 @@ public class Program {
 
 		Department obj = new Department (1, "Books");
 		
+		Seller seller = new Seller (1, "Bob", "bob@gmail.com", new Date(), 3000.0, obj); 
+		
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
