802a537f76f6586302a5ab1081a1a4d361f243b6
TiagoSouzaGarcia <tiago.s.garcia@gmail.com>
2021-03-10 13:34:26 -0300

    findAllImplemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,12 @@ public class Program {
 		for (Seller obj :list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: seller findByAll =====");
+		list = sellerDao.findAll();
+		for (Seller obj :list) {
+			System.out.println(obj);
+		}
 
 	}
 
