a4ec7f98fc4bd8ab712996b6450ed3197782c888
TiagoSouzaGarcia <tiago.s.garcia@gmail.com>
2021-03-10 15:06:44 -0300

    Updated DAOFactory, Created Test Program2, Created DepartmentDaoJDBC insert implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Date;
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("\n=== TEST 1: department insert =====");
+		Department newDepartment = new Department(null, "Shoes");
+		
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
+
+	}
+
+}
